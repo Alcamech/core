7fe89247a4dc6dba8cf7d79b0cec64249298673c
Lawton C. Mizell <alcamech@gmail.com>
2019-10-03 16:12:05 -0400

    added custom loom for dependency-graph fix, incremented stanford-corenlp version

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "Clojure wrapper for the Stanford CoreNLP tools."
   :url "https://github.com/clojurenlp/core"
   :license {:name "Apache License, Version 2.0"
-  	        :url "https://www.apache.org/licenses/LICENSE-2.0"}
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.8.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.8.0" 
+                 [edu.stanford.nlp/stanford-corenlp "3.9.2"]
+                 [edu.stanford.nlp/stanford-corenlp "3.9.2"
                   :classifier "models"]
                  [org.clojure/data.json "0.2.6"]
-                 [aysylu/loom "1.0.0"]
+                 [org.clojars.alcamech/loom "1.0.3"]
                  [org.slf4j/slf4j-simple "1.7.25"]]
   :plugins [[lein-auto "0.1.3"]])
